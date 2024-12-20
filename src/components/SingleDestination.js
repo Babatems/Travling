b2b1caaf4fefeda72e00f67f275f6ac3c1c4fa0a
Inumoh Temi <temiloluwainumoh@gmail.com>
2024-12-20 01:59:00 +0100

    Updated some the SingleDestination file

--- src/components/SingleDestination.js
+++ src/components/SingleDestination.js
@@ -3,12 +3,14 @@ import '@fontsource/lato';
 export default function SingleDestination({ image, location, title, duration, cost }) {
     return (
         <div className="w-full max-w-[400px] bg-[#F0F0F0] rounded-2xl overflow-hidden">
-            <div className="p-4 mb-4">
-                <img 
-                    src={image}
-                    alt={`${title}`}
-                    className="w-[264px] h-[180px] rounded-lg object-cover"
-                />
+            <div className="p-4">
+                <div className="flex justify-center mb-4">
+                    <img 
+                        src={image}
+                        alt={`${title}`}
+                        className="w-[264px] h-[180px] rounded-lg object-cover"
+                    />
+                </div>
 
                 <div className="mx-4">
                     <div className="flex items-center gap-2 mb-[9px]">
@@ -17,9 +19,18 @@ export default function SingleDestination({ image, location, title, duration, co
                             {location}
                         </p>
                     </div>
-                    <h3 className="font-bold text-[16px] leading-[140%] font-lato mb-1">{title}</h3>
-                    <p className="text-[#636363] text-[14px] leading-[140%] font-normal mb-4">{duration}</p>
-                    <p className="text-[13px] leading-[140%] font-bold text-[#42A7C3]">Rp {cost}/person</p>
+
+                    <div className="mb-2">
+                        <h3 className="font-bold text-[16px] leading-[140%] font-lato">{title}</h3>
+                    </div>
+
+                    <div className="mb-4">
+                        <p className="text-[#636363] text-[14px] leading-[140%] font-normal">{duration}</p>
+                    </div>
+
+                    <div>
+                        <p className="text-[13px] leading-[140%] font-bold text-[#42A7C3]">Rp {cost}/person</p>
+                    </div>
                 </div>
             </div>
         </div>
