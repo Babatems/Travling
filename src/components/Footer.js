7742033a5a937c08e77c11d76054d9bcf2f94798
Inumoh Temi <temiloluwainumoh@gmail.com>
2024-12-20 04:15:47 +0100

    Updated some files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div>
-            <img src="/images/CTA + Footer.png" alt="Footer" className="w-full h-[692px] object-contain"/>
+            <img src="/images/CTA + Footer.png" alt="Footer" className="w-full h-[692px] object-contain lg:h-full lg:object-cover"/>
         </div>
     )
 }
